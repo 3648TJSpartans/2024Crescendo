7cf93876f87510eba1fd0a235855973dd3d83acc
SamMcph <81268663+SamMcph@users.noreply.github.com>
2024-01-08 19:34:10 -0700

    updated

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -1,12 +1,9 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkLowLevel.MotorType;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants.ArmConstants;
 
 public class ArmSubsystem extends SubsystemBase {
+
     public ArmSubsystem() {
 
     }
