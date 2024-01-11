2036f8ada40f1c33ed07c08e87b61b4a075c0f9d
SamMcph <147000359+E-Latsis@users.noreply.github.com>
2024-01-11 14:34:15 -0700

    Fixed errors

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -4,11 +4,11 @@ import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
+import frc.robot.Constants.ArmConstants;
 public class ArmSubsystem extends SubsystemBase {
     private final CANSparkMax armMotor;
     public ArmSubsystem() {
-             armMotor = new CANSparkMax(armMotorId, MotorType.kBrushless);
+             armMotor = new CANSparkMax(ArmConstants.armMotorId, MotorType.kBrushless);
     }
 
     public void setAngle(double angle) {
