0f8e947f2f84c6734105bf67e4c72b3778520799
E-Latsis <147000359+E-Latsis@users.noreply.github.com>
2024-01-13 09:42:58 -0700

    IntakeButtonCmd finished

---
+++ src/main/java/frc/robot/commands/IntakeButtonCmd.java
@@ -0,0 +1,40 @@
+package frc.robot.commands;
+
+import java.util.function.Supplier;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.IntakeSubsystem;
+
+public class IntakeButtonCmd extends Command {
+    private final Supplier<Boolean> m_leftexecuteButton;
+    private final Supplier<Boolean> m_rightexecuteButton;
+    private final IntakeSubsystem IntakeSubsystem;
+
+    public IntakeButtonCmd(IntakeSubsystem IntakeSubsystem, Supplier<Boolean> m_leftexecuteButton,
+            Supplier<Boolean> m_rightexecuteButton) {
+        this.IntakeSubsystem = IntakeSubsystem;
+        this.m_leftexecuteButton = m_leftexecuteButton;
+        this.m_rightexecuteButton = m_rightexecuteButton;
+        addRequirements(IntakeSubsystem);
+    }
+
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public void execute() {
+        if (m_leftexecuteButton.get()) {
+            System.out.println(m_leftexecuteButton.get());
+            IntakeSubsystem.setIntakeSpeed(5);
+        } else if (m_rightexecuteButton.get()) {
+            System.out.println(m_rightexecuteButton.get());
+            IntakeSubsystem.setIntakeSpeed(-5);
+        } else {
+            IntakeSubsystem.setIntakeSpeed(0);
+        }
+
+    }
+
+}
--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -1,14 +1,25 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.IntakeContants;
 
 public class IntakeSubsystem extends SubsystemBase {
+    private final CANSparkMax intakeMotor1;
+    private final CANSparkMax intakeMotor2;
 
     public IntakeSubsystem() {
+
+        intakeMotor1 = new CANSparkMax(IntakeContants.Int1MotorId, MotorType.kBrushless);
+        intakeMotor2 = new CANSparkMax(IntakeContants.Int2MotorId, MotorType.kBrushless);
 
     }
 
     public void setIntakeSpeed(double speed) {
+        intakeMotor1.set(speed);
+        intakeMotor2.set(-speed);
 
     }
 
