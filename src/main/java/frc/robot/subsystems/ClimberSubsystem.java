ddf9eef2a1904cd767b681d0d55894c689f21180
SamMcph <81268663+SamMcph@users.noreply.github.com>
2024-01-13 14:31:18 -0700

    added PID controller

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -4,6 +4,7 @@ import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.SparkAbsoluteEncoder.Type;
 import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.SparkPIDController;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ArmConstants;
@@ -11,10 +12,13 @@ import frc.robot.Constants.ArmConstants;
 public class ClimberSubsystem extends SubsystemBase {
     private final CANSparkMax m_climberMotor;
     private final AbsoluteEncoder m_climberEncoder;
+    private final SparkPIDController m_climberPIDController;
 
     public ClimberSubsystem() {
         m_climberMotor = new CANSparkMax(ArmConstants.armMotorId, MotorType.kBrushless);
         m_climberEncoder = m_climberMotor.getAbsoluteEncoder(Type.kDutyCycle);
+        m_climberPIDController = m_climberMotor.getPIDController();
+        m_climberPIDController.setFeedbackDevice(m_climberEncoder);
     }
 
     public void setClimberPosition(double position) {
