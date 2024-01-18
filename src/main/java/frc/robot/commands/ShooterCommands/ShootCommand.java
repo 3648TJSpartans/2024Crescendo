5b84eccac7c71cceab0174f8f97bf91e44804576
E-Latsis <147000359+E-Latsis@users.noreply.github.com>
2024-01-17 17:06:34 -0700

    Put all the commands for the shooter in a folder

--- src/main/java/frc/robot/commands/ShooterCommands/ShootCommand.java
+++ src/main/java/frc/robot/commands/ShooterCommands/ShootCommand.java
@@ -1,4 +1,4 @@
-package frc.robot.commands;
+package frc.robot.commands.ShooterCommands;
 
 import java.util.function.Supplier;
 
@@ -7,11 +7,11 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants.ShooterConstants;
 import frc.robot.subsystems.ShooterSubsystem;
 
-public class RevMotorCommand extends Command {
+public class ShootCommand extends Command {
     private final Supplier<Boolean> m_ShootButton;
     private final ShooterSubsystem ShooterSubsystem;
 
-    public RevMotorCommand(ShooterSubsystem m_ShooterSubsystem, Supplier<Boolean> m_ShootButton) {
+    public ShootCommand(ShooterSubsystem m_ShooterSubsystem, Supplier<Boolean> m_ShootButton) {
         ShooterSubsystem = m_ShooterSubsystem;
         this.m_ShootButton = m_ShootButton;
         addRequirements(m_ShooterSubsystem);
@@ -27,6 +27,7 @@ public class RevMotorCommand extends Command {
     public void execute() {
         if (m_ShootButton.get()) {
             ShooterSubsystem.revShooter();
+            ShooterSubsystem.moveShooterIntake();
 
         }
 
