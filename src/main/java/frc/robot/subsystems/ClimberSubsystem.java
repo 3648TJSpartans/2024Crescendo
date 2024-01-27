012e4676ac3f394f4da6e34d560b692c11f0064d
Michael Benissan <MichaelBenissan@gmail.com>
2024-01-27 12:36:39 -0700

    Created initial steps for allowing PID functions to command the Climber subsystem

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -4,10 +4,15 @@ import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.SparkPIDController;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj2.command.PIDSubsystem;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ClimberConstants;
 
-public class ClimberSubsystem extends SubsystemBase {
+public class ClimberSubsystem extends PIDSubsystem {
     private final CANSparkMax m_climberMotor1;
     private final CANSparkMax m_climberMotor2;
     private final SparkPIDController m_climberPIDController1;
@@ -15,7 +20,18 @@ public class ClimberSubsystem extends SubsystemBase {
     private final RelativeEncoder m_climberEncoder1;
     private final RelativeEncoder m_climberEncoder2;
 
+    //For the PID Subsystem
+    private final Encoder m_cEncoder1 = new Encoder(0, 0, 0);
+    private final Encoder m_cEncoder2 = new Encoder(0, 0, 0);
+    private final SimpleMotorFeedforward m_Feedforward = new SimpleMotorFeedforward(0, 0);
+
     public ClimberSubsystem() {
+        super(new PIDController(ClimberConstants.kClimberP, ClimberConstants.kClimberI, ClimberConstants.kClimberD));
+        setSetpoint(ClimberConstants.kClimberHeight);
+        // The zeros are magic numbers that are placeholders until more info is obtained about "setDistancePerPulse"
+        m_cEncoder1.setDistancePerPulse(0);
+        m_cEncoder2.setDistancePerPulse(0);
+
         // Motor 1
         m_climberMotor1 = new CANSparkMax(ClimberConstants.climberMotor1ID, MotorType.kBrushless);
         m_climberEncoder1 = m_climberMotor1.getEncoder();
@@ -52,5 +68,23 @@ public class ClimberSubsystem extends SubsystemBase {
     public void MoveClimber(double speed) {
         m_climberMotor1.set(speed);
         m_climberMotor2.set(speed);
+    }
+
+
+    // These two methods are created for the PID subsystem; WIll continue research on these methods.
+    @Override
+    protected void useOutput(double output, double setpoint) {
+        // TODO Auto-generated method stub
+        double Voltage = output + m_Feedforward.calculate(setpoint);
+        m_climberMotor1.setVoltage(Voltage);
+        m_climberMotor2.setVoltage(Voltage);
+        throw new UnsupportedOperationException("Unimplemented method 'useOutput'");
+    }
+
+    @Override
+    protected double getMeasurement() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getMeasurement'");
+        return m_controller.getRate();
     }
 }
