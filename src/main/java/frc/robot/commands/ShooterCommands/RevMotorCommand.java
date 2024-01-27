a0da9c560073fa6ab9c2da7430cf38f58d6ebbc5
E-Latsis <147000359+E-Latsis@users.noreply.github.com>
2024-01-27 09:48:29 -0700

    IR Sensor in Shooter Subsytem and RevMotor Command

--- src/main/java/frc/robot/commands/ShooterCommands/RevMotorCommand.java
+++ src/main/java/frc/robot/commands/ShooterCommands/RevMotorCommand.java
@@ -6,24 +6,35 @@ import frc.robot.subsystems.ShooterSubsystem;
 
 public class RevMotorCommand extends Command {
     private final ShooterSubsystem m_shooterSubsystem;
+    private boolean finished;
 
     public RevMotorCommand(ShooterSubsystem shooterSubsystem) {
         m_shooterSubsystem = shooterSubsystem;
         addRequirements(m_shooterSubsystem);
     }
+}
 
     @Override
     public void initialize() {
+        finished = false;
 
     }
 
     @Override
     public void execute() {
-        m_shooterSubsystem.revShooter(ShooterConstants.motorSpeed);
+        if (NoteLocaiton()){
+            m_shooterSubsystem.revShooter(ShooterConstants.motorSpeed);
+            finished = true;
+        }
     }
 
     @Override
     public void end(boolean interrupted) {
         m_shooterSubsystem.revShooter(0);
     }
-}
+    @Ovveride
+    public boolean isFinished() {
+
+    }
+
+
