07b2dc5bb34dc0e86670de580b822184f11ec969
SamMcph <81268663+SamMcph@users.noreply.github.com>
2024-01-31 14:48:54 -0700

    started to add logging to shuffleboard

---
+++ src/main/java/frc/robot/Utils/ShuffleBoardSubsystem.java
@@ -0,0 +1,12 @@
+package frc.robot.Utils;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ShuffleBoardSubsystem {
+
+    private String m_subsystemName;
+
+    public ShuffleBoardSubsystem(String subsystemName) {
+        m_subsystemName = subsystemName;
+    }
+}
