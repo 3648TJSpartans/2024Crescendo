c7730a2392ac4d8c8bb508cb52c502e7074fc569
SamMcph <81268663+SamMcph@users.noreply.github.com>
2024-01-31 11:36:46 -0700

    added time out cmds to endgame and added endgame constants

--- src/main/java/frc/robot/commands/Endgame/EndgameCmdGroup.java
+++ src/main/java/frc/robot/commands/Endgame/EndgameCmdGroup.java
@@ -3,6 +3,7 @@ package frc.robot.commands.Endgame;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Constants.ClimberConstants;
+import frc.robot.Constants.EndgameConstants;
 import frc.robot.Constants.TrapConstants;
 import frc.robot.subsystems.ClimberSubsystem;
 import frc.robot.subsystems.TrapSubsystem;
@@ -14,12 +15,17 @@ public class EndgameCmdGroup extends SequentialCommandGroup {
     public EndgameCmdGroup(TrapSubsystem trapSubsystem, ClimberSubsystem climberSubsystem) {
         m_trapSubsystem = trapSubsystem;
         m_climberSubsystem = climberSubsystem;
-        addCommands(new EndgameClimberCmd(m_climberSubsystem, ClimberConstants.kClimberDown),
-                new WaitCommand(TrapConstants.kTrapTime),
-                new EndgameUpDownCmd(m_trapSubsystem, TrapConstants.kpositionUpDown),
-                new WaitCommand(TrapConstants.kTrapTime),
-                new EndgameInOutCmd(m_trapSubsystem, TrapConstants.kpositionInOut),
-                new WaitCommand(TrapConstants.kTrapTime),
-                new EndgameTrackCmd(m_trapSubsystem, TrapConstants.kspeed));
+        addCommands(
+                new EndgameClimberCmd(m_climberSubsystem, ClimberConstants.kClimberDown)
+                        .withTimeout(EndgameConstants.kclimberTime),
+                new WaitCommand(EndgameConstants.waitTime),
+                new EndgameUpDownCmd(m_trapSubsystem, TrapConstants.kpositionUpDown)
+                        .withTimeout(EndgameConstants.kTrapUpTime),
+                new WaitCommand(EndgameConstants.waitTime),
+                new EndgameInOutCmd(m_trapSubsystem, TrapConstants.kpositionInOut)
+                        .withTimeout(EndgameConstants.kTrapOutTime),
+                new WaitCommand(EndgameConstants.waitTime),
+                new EndgameTrackCmd(m_trapSubsystem, TrapConstants.kspeed)
+                        .withTimeout(EndgameConstants.kTrapReleaseTime));
     }
 }
