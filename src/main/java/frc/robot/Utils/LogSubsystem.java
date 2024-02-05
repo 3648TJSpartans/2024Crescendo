a17fd46b042237bd8f6f42880b107c072e9cfe1c
BigBoiGishath <155850691+IdrTin@users.noreply.github.com>
2024-02-05 15:08:11 -0700

    Datalogging Moved to Robot.java
    - Creates a new Datalog once robotInit is called (when robot turns on)

--- src/main/java/frc/robot/Utils/LogSubsystem.java
+++ src/main/java/frc/robot/Utils/LogSubsystem.java
@@ -5,20 +5,13 @@ import edu.wpi.first.util.datalog.DoubleLogEntry;
 import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class LogSubsystem extends SubsystemBase{
+public class LogSubsystem extends SubsystemBase {
     DoubleLogEntry m_EncoderLog;
 
-    public LogSubsystem(String subsystemName){
+    public LogSubsystem(String subsystemName) {
     }
 
-    @Override
-    public void robotInit(){
-        DataLogManager.start();
-        DataLog log = DataLogManager.getLog();
-        m_EncoderLog = new DoubleLogEntry(log, "Encoder Value");
-    }
-
-    public double logValue(double value){
+    public double logValue(double value) {
         m_EncoderLog.append(value);
         return value;
     }
