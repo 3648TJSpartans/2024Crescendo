0abfdc0b79eb62ac0a778195cc33353d437d86f8
BigBoiGishath <155850691+IdrTin@users.noreply.github.com>
2024-02-16 08:41:05 -0700

    Log Subsystem Update

--- src/main/java/frc/robot/Utils/LogSubsystem.java
+++ src/main/java/frc/robot/Utils/LogSubsystem.java
@@ -6,13 +6,19 @@ import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LogSubsystem extends SubsystemBase {
-    DoubleLogEntry m_EncoderLog;
+    private final DoubleLogEntry m_EncoderLog;
+    private final DataLog m_Log;
 
     public LogSubsystem(String subsystemName) {
+        // Starts recording to data log
+        DataLogManager.start();
+
+        // Setup custom log entries
+        m_Log = DataLogManager.getLog();
+        m_EncoderLog = new DoubleLogEntry(m_Log, subsystemName);
     }
 
-    public double logValue(double value) {
+    public void logValue(double value) {
         m_EncoderLog.append(value);
-        return value;
     }
 }
