3738797f8a492b609f08452cff3e1d10227c319c
Avery Vinlove <144643328+rakdosintolerant@users.noreply.github.com>
2024-02-25 21:17:57 -0700

    Update LedsSubsystem.java
    Decided to try some fancy-looking lights after being inspired online and at the competition.

--- src/main/java/frc/robot/subsystems/LedsSubsystem.java
+++ src/main/java/frc/robot/subsystems/LedsSubsystem.java
@@ -8,12 +8,16 @@ import frc.robot.Constants.LedConstants;
 import org.ejml.interfaces.decomposition.LUSparseDecomposition;
 
 import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.Timer;
 
 public class LedsSubsystem extends SubsystemBase {
 
     private final AddressableLEDBuffer m_ledBuffer;
 
     private final AddressableLED m_led;
+
+    private Timer time = new Timer(); //Not sure if there is a better timer to use
+    int count = 0; //Also maybe not wise
 
     public LedsSubsystem() {
         m_ledBuffer = new AddressableLEDBuffer(LedConstants.ledLength);
@@ -44,4 +48,42 @@ public class LedsSubsystem extends SubsystemBase {
         }
 
     }
+
+    //Should be called periodically, ramp could be lower when shooting?
+    //Some numbers should be changed with constants
+    public void pewPew(double ramp) {
+        if (time.get() > ramp) {
+          setColor(0, 127, 124, 0, 30);
+          if (count < 22) {setColor(0, 124, 129, count, count+8);}
+          else {
+            setColor(0, 124, 129, count, 30);
+            setColor(0, 124, 129, 0, (count+8)%30);
+          }
+          if (count < 23) {setColor(0, 118, 131, count+1, count+7);}
+          else if (count < 29) {
+            setColor(0, 118, 131, count+1, 30);
+            setColor(0, 118, 131, 0, (count+7)%30);
+          }
+          if (count < 24) {setColor(0, 104, 144, count+2, count+6);}
+          else if (count < 28) {
+            setColor(0, 104, 144, count+2, 30);
+            setColor(0, 104, 144, 0, (count+6)%30);
+          }
+          if (count < 25) {setColor(0, 92, 152, count+3, count+5);}
+          else if (count < 27) {
+            setColor(0, 92, 152, count+3, 30);
+            setColor(0, 92, 152, 0, (count+5)%30);
+          }
+          if (count < 26) {setColor(0, 87, 160, count+4, count+4);}
+          else {
+            setColor(0, 87, 160, 0, (count+4)%30);
+          }
+          count++;
+          if (count > 30) {
+            count = 0;
+          }
+          time.reset();
+          time.start();
+        }
+      }
 }
